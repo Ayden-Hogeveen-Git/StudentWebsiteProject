e17dd3b02493fbf16357d3537f634f4ed09af3c7
Ayden Hogeveen <a.hogeveen@share.epsb.ca>
2023-02-19 19:08:16 -0700

    Couple more updates, gonna watch the NBA all-star game

---
+++ js/iconMoveScript.js
@@ -0,0 +1,12 @@
+/*
+title: Social Media Link Icon Movement Script
+author: Ayden Hogeveen
+data-created: 2023-02-19
+*/
+
+var iconPos = document.getElementsByClassName("links-div");
+window.addEventListener("scroll", function(){
+    console.log("Scrolling");
+    // this.document.documentElement.style.setProperty('--icon-offset', '1100px');
+    // TODO: smoothly move 2 icons left with scroll, 2 icons right
+})
